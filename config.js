24f6e33d552401cf10d33e5178302e79275ef4db
Quốc Đạt <68956719+badaoboy99@users.noreply.github.com>
2023-03-08 15:27:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Em';
-const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const nameGirl = 'Cừu Béo';
+const giftUrl = '.';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng Cừu Béo';
+const contentCard = 'Chúc Cừu Béo ngày 8/3 vui vẻ, Xinh hơn, Iu a nhiều hơn';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
